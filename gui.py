6656f0d49de42c0cca34289f81a0474b198f3e3c
SimonRisso <simonrisso.e@gmail.com>
2024-07-03 15:09:55 -0300

    Implementación del botón de añadir

--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Ingrese una tarea")
-input_box = sg.InputText(tooltip="Escriba la tarea")
+input_box = sg.InputText(tooltip="Escriba la tarea", key="tarea")
 add_button = sg.Button("Agregar")
 
-window = sg.Window('Mi aplicación de tareas por hacer', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('Mi aplicación de tareas por hacer',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Agregar":
+            tasks = functions.get_tasks()
+            new_task = values['tarea'] + "\n"
+            tasks.append(new_task)
+            functions.write_tasks(tasks)
+        case sg.WIN_CLOSED:
+            break
+        
 window.close()
