e75d7244cfa7363a28edc6c49baf782fca110258
SimonRisso <simonrisso.e@gmail.com>
2024-07-08 10:58:20 -0300

    Cambios en el tema, agregado de reloj y solución de errores a los botones de "editar" y "completada"

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkBrown6")
+
+clock = sg.Text("", key="clock")
 label = sg.Text("Ingrese una tarea")
 input_box = sg.InputText(tooltip="Escriba la tarea", key="tarea")
-add_button = sg.Button("Agregar")
+add_button = sg.Button("Agregar", size=10)
 list_box = sg.Listbox(values=functions.get_tasks(), key="tareas",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Editar")
@@ -11,17 +15,16 @@ complete_button = sg.Button("Completada")
 exit_button = sg.Button("Salir")
 
 window = sg.Window('Mi aplicación de tareas por hacer',
-                   layout=[[label], 
+                   layout=[[clock],
+                           [label], 
                            [input_box, add_button], 
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values["tareas"])
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Agregar":
             tasks = functions.get_tasks()
@@ -31,22 +34,28 @@ while True:
             window["tareas"].update(values=tasks)
             
         case "Editar":
-            task_to_edit = values["tareas"][0]
-            new_task = values["tarea"]
-            
-            tasks = functions.get_tasks()
-            index = tasks.index(task_to_edit)
-            tasks[index] = new_task
-            functions.write_tasks(tasks)
-            window["tareas"].update(values=tasks)
+            try:
+                task_to_edit = values["tareas"][0]
+                new_task = values["tarea"]
+                
+                tasks = functions.get_tasks()
+                index = tasks.index(task_to_edit)
+                tasks[index] = new_task
+                functions.write_tasks(tasks)
+                window["tareas"].update(values=tasks)
+            except IndexError:
+                sg.popup("Por favor, primero seleccione una tarea", font=("Helvetica", 20))
         case "Completada":
-            task_completed = values["tareas"][0]
-            
-            tasks = functions.get_tasks()
-            tasks.remove(task_completed)
-            functions.write_tasks(tasks)
-            window["tareas"].update(values=tasks)
-            window["tarea"].update(value="")
+            try:
+                task_completed = values["tareas"][0]
+                
+                tasks = functions.get_tasks()
+                tasks.remove(task_completed)
+                functions.write_tasks(tasks)
+                window["tareas"].update(values=tasks)
+                window["tarea"].update(value="")
+            except IndexError:
+                sg.popup("Por favor, primero seleccione una tarea", font=("Helvetica", 20))
         case "Salir":
             break
         case "tareas":
