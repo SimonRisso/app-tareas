9019547398ed035ca615a9198662432c139d8c0e
SimonRisso <simonrisso.e@gmail.com>
2024-07-09 11:45:33 -0300

    Implementación de función para agregar nuevos items

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 tasks = functions.get_tasks()
 
+def add_task():
+    task = st.session_state["new_task"] + "\n"
+    print(task)
+    tasks.append(task)
+    functions.write_tasks(tasks)
+
 st.title("My todo App")
 st.subheader("Esta es mi aplicación de cosas por hacer")
 st.write("Está diseñada para aumentar tu productividad")
@@ -10,4 +16,6 @@ st.write("Está diseñada para aumentar tu productividad")
 for task in tasks:
     st.checkbox(task)
     
-st.text_input(label="", placeholder="Agregue una tarea...")
+st.text_input(label="", placeholder="Agregue una tarea...", on_change=add_task, key="new_task")
+
+st.session_state
